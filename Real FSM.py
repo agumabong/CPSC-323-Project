f58b34b3da9521379cf3b3916bdad296ebf7878f
agumabong <32043546+agumabong@users.noreply.github.com>
2018-11-10 03:15:25 -0800

    Update Real FSM.py
    Made rudimentary lexer function for the syntax analyzer.

--- Real FSM.py
+++ Real FSM.py
@@ -1,3 +1,8 @@
+#Aaron Gumabong
+#Dan Ortiz
+#CPSC 323
+#Professor Choi
+ 
 #Array to hold the integer FSM
 intlist = [2,2]
 #Function takes in a word and checks if the word is an Integer. 
@@ -74,100 +79,131 @@ def real(word):
         return False
 
 #Lists to store the different types of keywords, operators and seperators
-keywords = ["if", "while", "for", "whileend", "else", "ifend", "whileend", "get", "put", "and", "True", "False"]
+keywords = ["if", "while", "for", "whileend", "else", "ifend", "whileend", "get", "put", "and", "true", "false"]
 operators = ["+","-","*","/","%", "=", "<",">", "&", "|"]
-sep = ["(", ")", "[", "]", "{","}"]
+sep = ["(", ")", "[", "]", "{","}", "$$"]
 
-#Lexer function. Takes in the name of a file and reads each line and separates them into tokens and lexemes. 
-def lexer(file_name):
+#New form of lexer that takes in a token and returns the token type
+def lexer(token):
+    if (token in keywords):
+        print("keyword")
+        return "keyword"
+    elif (token in operators):
+        print("operator")
+        return "operator"
+    elif (token in "sep"):
+        print("separator")
+        return seperator
+    elif (real(token) == True):
+        print("real")
+        return "real"
+    elif (iden(token) == True):
+        print("identifier")
+        return "identifier"
+    elif (integer(token) == True):
+        print("integer")
+        return "integer"
+
+###Lexer function. Takes in the name of a file and reads each line and separates them into tokens and lexemes. 
+##def lexer(file_name):
+##
+##    
+##    #open the file
+##    f=open(file_name,"r")
+##    #Store each line of the file into an array
+##    content = f.read().split("\n")    
+##    
+##    print("Lexeme" + "\t" + "token")
+##    print("----------"+"\t" + "--------")
+##    
+##    #Loops j amount of times, j being the number of lines in the file.
+##    for j in range(0,len(content)):     
+##        
+##        #Split each individual line into words separated by a space and put it in a list.
+##        clist = content[j].split(" ")
+##       
+##        #Parse each line and print out what each token is.
+##        print("\n")
+##        comment = False
+##        for k in range(0, len(clist)):
+##            
+##            #Variable used to identify separators. 
+##            pword = clist[k]
+##            #If there is an empty line skip the line. 
+##            if len(pword) == 0:
+##                continue
+##            #Check if the symbol is a comment. if it is, then every subsequent token will be a comment until "*]" is reached.
+##            
+##            if(pword[:2] == "[*"):
+##                print(pword[:2] + "\t" + "Comment")
+##                print(pword[2:] + "\t" + "Comment")
+##                comment = True
+##                continue
+##
+##            if(pword[-2:] == "*]"):
+##                print(pword[-2:] + "\t" + "Comment")
+##                print(pword[:-2] + "\t" + "Comment")
+##                comment = False
+##                continue
+##
+##            if (comment == True):
+##                print(clist[k] + "\t" + "Comment")
+##                continue
+##            
+##            
+##            #Check to see if there is an seperator at the beginning of the word. If there is, separate it from the word and print out the seperator and then the token that encapsulates it.
+##            if (pword[0] in sep):
+##                print(pword[0] + "\t" + "Seperator")
+##                pword = pword[1:]
+##                if (iden(pword) == True):
+##                    print(pword + "\t" + "Identifier")
+##                elif(real(pword) == True):
+##                     print(pword + "\t" + "real")
+##                elif(integer(pword) == True):
+##                     print(pword + "\t" + "Integer")
+##            #Check to see if there is an seperator at the end of the word. If there is, separate it from the word and print out the seperator and then the token that encapsulates it.
+##            elif(pword[len(pword)-1] in sep):
+##                pend = pword[len(pword)-1]            
+##                pword = pword[:-1]
+##                if (iden(pword) == True):
+##                    print(pword + "\t" + "Identifier")
+##                elif(real(pword) == True):
+##                     print(pword + "\t" + "real")
+##                elif(integer(pword) == True):
+##                     print(pword + "\t" + "Integer")
+##                print(pend + "\t" + "Seperator")
+##                
+##            #Checks if the word is an operator.
+##            elif (clist[k] in operators):
+##                print(clist[k] + "\t" + "Operator")
+##                
+##                
+##            #Checks if the word is a keyword.
+##            elif (clist[k] in keywords):
+##                print(clist[k] + "\t" + "Keyword")
+##               
+##                
+##            #Checks if the word is an Identifier.
+##            elif (iden(clist[k]) == True):
+##                print(clist[k] + "\t" + "Identifier")
+##              
+##                
+##            #Checks if the word is a real.
+##            elif(real(clist[k]) == True):
+##                 print(clist[k] + "\t" + "Real")
+##               
+##                 
+##            #Check if the word is an Integer
+##            elif(integer(clist[k]) == True):                 
+##                 print(clist[k] + "\t" + "Integer")
+##               
+##            #If the token fits none of the above values, return invalid token.
+##            else:
+##                print(clist[k] + "\t" + "Invalid Token")
+
 
     
-    #open the file
-    f=open(file_name,"r")
-    #Store each line of the file into an array
-    content = f.read().split("\n")    
-    
-    print("Lexeme" + "\t" + "token")
-    print("----------"+"\t" + "--------")
-    
-    #Loops j amount of times, j being the number of lines in the file.
-    for j in range(0,len(content)):     
-        
-        #Split each individual line into words separated by a space and put it in a list.
-        clist = content[j].split(" ")
-       
-        #Parse each line and print out what each token is.
-        print("\n")
-        comment = False
-        for k in range(0, len(clist)):
-            
-            #Variable used to identify separators. 
-            pword = clist[k]
-            #If there is an empty line skip the line. 
-            if len(pword) == 0:
-                continue
-            #Check if the symbol is a comment. if it is, then every subsequent token will be a comment until "*]" is reached.
-            
-            if(pword[:2] == "[*"):
-                print(pword[:2] + "\t" + "Comment")
-                print(pword[2:] + "\t" + "Comment")
-                comment = True
-                continue
 
-            if(pword[-2:] == "*]"):
-                print(pword[-2:] + "\t" + "Comment")
-                print(pword[:-2] + "\t" + "Comment")
-                comment = False
-                continue
-
-            if (comment == True):
-                print(clist[k] + "\t" + "Comment")
-                continue
-            
-            
-            #Check to see if there is an seperator at the beginning of the word. If there is, separate it from the word and print out the seperator and then the token that encapsulates it.
-            if (pword[0] in sep):
-                print(pword[0] + "\t" + "Seperator")
-                pword = pword[1:]
-                if (iden(pword) == True):
-                    print(pword + "\t" + "Identifier")
-                elif(real(pword) == True):
-                     print(pword + "\t" + "real")
-                elif(integer(pword) == True):
-                     print(pword + "\t" + "Integer")
-            #Check to see if there is an seperator at the end of the word. If there is, separate it from the word and print out the seperator and then the token that encapsulates it.
-            elif(pword[len(pword)-1] in sep):
-                pend = pword[len(pword)-1]            
-                pword = pword[:-1]
-                if (iden(pword) == True):
-                    print(pword + "\t" + "Identifier")
-                elif(real(pword) == True):
-                     print(pword + "\t" + "real")
-                elif(integer(pword) == True):
-                     print(pword + "\t" + "Integer")
-                print(pend + "\t" + "Seperator")
-                
-            #Checks if the word is an operator.
-            elif (clist[k] in operators):
-                print(clist[k] + "\t" + "Operator")
-                
-            #Checks if the word is a keyword.
-            elif (clist[k] in keywords):
-                print(clist[k] + "\t" + "Keyword")
-                
-            #Checks if the word is an Identifier.
-            elif (iden(clist[k]) == True):
-                print(clist[k] + "\t" + "Identifier")
-                
-            #Checks if the word is a real.
-            elif(real(clist[k]) == True):
-                 print(clist[k] + "\t" + "Real")
-                 
-            #Check if the word is an Integer
-            elif(integer(clist[k]) == True):                 
-                 print(clist[k] + "\t" + "Integer")
-            #If the token fits none of the above values, return invalid token.
-            else:
-                print(clist[k] + "\t" + "Invalid Token")
-                
-lexer("test.txt")
+##file = input("Please enter a file name: ")
+##lexer(file)
+##q = (input("\n Press any key to stop."))
