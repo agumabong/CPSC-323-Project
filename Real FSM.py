ea29ef5b2cfea794e99c97e2de5d3430c3bbac04
dbortiz <dbortiz@csu.fullerton.edu>
2018-11-12 00:47:29 -0800

    Created some rules. Not complete.

--- Real FSM.py
+++ Real FSM.py
@@ -2,10 +2,17 @@
 #Dan Ortiz
 #CPSC 323
 #Professor Choi
- 
+
+
+# Variables to keep track of top-down parser
+current_lexeme = None
+global list_of_lexemes
+global position_in_list
+
+
 #Array to hold the integer FSM
 intlist = [2,2]
-#Function takes in a word and checks if the word is an Integer. 
+#Function takes in a word and checks if the word is an Integer.
 def integer(word):
     #Intializing starting state and column to default values
     state = 1
@@ -45,9 +52,9 @@ def iden(word):
         return True
     #If it is in neither of those states, return False.
     else:
-        return False                   
-        
-    
+        return False
+
+
 
 #Converts the character input to the column number for the Real FSM. Valid inputs are digits and ".".
 def ch_to_col(c):
@@ -67,14 +74,14 @@ def real(word):
     for i in range(0,len(word)):
         #Checks if the current letter is a number or "."
         #If it is not, then automatically return False
-        if (word[i].isdigit() or word[i] == "."):  
+        if (word[i].isdigit() or word[i] == "."):
             #print ("Char is: " + str(word[i]) + " and state is: " + str(state))
             state = realarray[state-1][ch_to_col(word[i])]
         else:
             return False
     #Only accepting state is 4. If the state is not 4 then the function will return False.
     if (state == 4):
-        return True    
+        return True
     else:
         return False
 
@@ -93,7 +100,7 @@ def lexer(token):
         return "operator"
     elif (token in "sep"):
         print("separator")
-        return seperator
+        return "separator"
     elif (real(token) == True):
         print("real")
         return "real"
@@ -104,7 +111,212 @@ def lexer(token):
         print("integer")
         return "integer"
 
-###Lexer function. Takes in the name of a file and reads each line and separates them into tokens and lexemes. 
+"""
+def rat18f():
+    # Maybe print out the file?
+    print("<Rat18F> -> <Opt Function Definitions> $$ <Opt Declaration List> <Statement List> $$")
+
+
+    opt_function_definition()
+
+
+    if current_lexeme == "$$":
+        get_next_lexeme()
+"""
+
+
+def qualifier():
+    global current_lexeme
+    print("<Qualifier -> int | boolean | real")
+
+    if current_lexeme.isdigit():
+        print("int")
+    elif current_lexeme == "true" or current_lexeme == "false":
+        print("boolean")
+    else:
+        print("real")
+
+
+def IDs():
+    global current_lexeme
+    print("<IDs> -> <Identifier> | <Identifier>, <IDs>")
+
+    position_in_list += 1
+    if list_of_lexemes[position_in_list] == ",":
+        IDs()
+    else:
+        position_in_list += 1
+        current_lexeme = list_of_lexemes[position_in_list]
+
+
+def statement_list():
+    print("<Statement List> -> <Statement> | <Statement> <Statement List")
+
+    while True:
+        statement()
+
+
+def statement():
+    global current_lexeme
+
+    print("<Statement -> <Compound> | <Assign> | <If> |  <Return> | <Print> | <Scan> | <While>")
+    current_lexeme = list_of_lexemes[position_in_list]
+    # Separate transitions for each case
+    if current_lexeme == "{":
+        compound()
+    elif lexer(current_lexeme) == "identifier":
+        assign()
+
+    # elif current_lexeme == "if":
+     #   If()
+    #elif current_lexeme == "return":
+       # Return()
+    #elif current_lexeme == "put":
+      #  Print()
+    #elif current_lexeme == "get":
+     #   Scan()
+    # elif current_lexeme == "while":
+        # While()
+    else:
+        print("Error in statement() function")
+
+
+def compound():
+    print("<Compound> -> { <Statement List> }")
+
+    statement_list()
+
+
+def assign():
+    global current_lexeme
+    print("<Assign> -> <Identifier> = <Expression> ;")
+
+    if lexer(current_lexeme) == "identifier":
+        if list_of_lexemes[position_in_list + 1] == "=":
+            position_in_list += 1
+            current_lexeme = list_of_lexemes[position_in_list]
+            expression()
+
+            if current_lexeme != ";":
+                print("Missing ;")
+    else:
+        print("There's in error in assign()")
+
+
+def expression():
+    print("<Expression> -> <Term> <Expression Prime>")
+
+    term()
+    expression_prime()
+
+
+def expression_prime():
+    global current_lexeme
+    print("<Expression Prime> -> + <Term> <Expression> | - <Term> <Expression> | epsilon")
+
+    if current_lexeme == "+" or current_lexeme == "-":
+        position_in_list += 1
+        current_lexeme = list_of_lexemes[position_in_list]
+        term()
+        expression()
+    else:
+        print("epsilon")
+
+
+def term():
+    print("<Term> -> <Factor> <Term Prime>")
+
+    factor()
+    term_prime()
+
+
+def term_prime():
+    global current_lexeme
+    print("<Term Prime> -> * <Factor> <Term> | / <Factor> <Term> | epsilon")
+
+    if current_lexeme == "*" or current_lexeme == "/":
+        position_in_list += 1
+        current_lexeme = list_of_lexemes[position_in_list]
+        factor()
+        term()
+    else:
+        print("epsilon")
+
+
+def factor():
+    global current_lexeme
+    print("<Factor> -> - <Primary> | <Primary>")
+
+    if current_lexeme == "-":
+        position_in_list += 1
+        current_lexeme = list_of_lexemes[position_in_list]
+        primary()
+    else:
+        primary()
+
+
+def primary():
+    print("<Primary> -> <Identifier> | <Integer> | <Identifier> ( <IDs> ) | ( <Expression> | <Real> | true | false")
+    global current_lexeme
+    if lexer(current_lexeme) == "identifier":
+        if list_of_lexemes[position_in_list + 1] == "(":
+            position_in_list += 1
+            current_lexeme = list_of_lexemes[position_in_list]
+            IDs()
+            if list_of_lexemes[position_in_list + 1]  != ")":
+                print("Missing ')'")
+        else:
+            position_in_list += 1
+            current_lexeme = list_of_lexemes[position_in_list]
+    elif lexer(current_lexeme) == "integer":
+        position_in_list += 1
+        current_lexeme = list_of_lexemes[position_in_list]
+    elif current_lexeme == "(":
+        expression()
+        if list_of_lexemes[position_in_list + 1] != ")":
+            print("Missing ')'")
+        else:
+            position_in_list += 1
+            current_lexeme = list_of_lexemes[position_in_list]
+    elif current_lexeme == "real":
+        position_in_list += 1
+        current_lexeme = list_of_lexemes[position_in_list]
+    elif current_lexeme == "false":
+        position_in_list += 1
+        current_lexeme = list_of_lexemes[position_in_list]
+    else:
+        print("Error in primary function")
+
+
+# Main Program
+text = "a = b + c"
+list_of_lexemes = text.split(" ")
+position_in_list = 0
+
+
+statement()
+
+"""
+def If():
+    print("")
+
+def opt_function_definition():
+    print("opt_function_defintion")
+    function_definition()
+    # empty()
+
+def function_definition():
+    print("function_definition")
+    #if
+    func()
+    #else:
+     #   func()
+      #  function_definition()
+"""
+
+def empty():
+    print("<Empty> -> epsilon")
+###Lexer function. Takes in the name of a file and reads each line and separates them into tokens and lexemes.
 ##def lexer(file_name):
 ##
 ##    
@@ -202,7 +414,7 @@ def lexer(token):
 ##                print(clist[k] + "\t" + "Invalid Token")
 
 
-    
+
 
 ##file = input("Please enter a file name: ")
 ##lexer(file)
